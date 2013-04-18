561e8378845efe7ea78b308d5830ba802e05b507
Arnold Zokas <arnold.zokas@coderoom.net>
2013-04-18 01:46:14 +0100

    Applied GlobalAssemblyInfo pattern to all projects

--- Coderoom.Site/Properties/AssemblyInfo.cs
+++ Coderoom.Site/Properties/AssemblyInfo.cs
@@ -1,13 +1,3 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Coderoom.Site")]
-[assembly: AssemblyProduct("http://coderoom.net/")]
-[assembly: AssemblyCopyright("Copyright © Arnold Zokas 2013")]
-
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("http://coderoom.net/")]
+[assembly: AssemblyCopyright("Copyright © Arnold Zokas 2013")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
