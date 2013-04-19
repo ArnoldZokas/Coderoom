6228ad661797c3962db97f050c241c7beffc63a2
Arnold Zokas <arnold.zokas@coderoom.net>
2013-04-19 12:01:25 +0200

    Implemented proper build error reporting

--- build/build.ps1
+++ build/build.ps1
@@ -1,23 +1,29 @@
 Framework "4.0"
 
 Properties {
-    $build_dir = split-path $psake.build_script_file
-    $project_dir = "$build_dir\.."
-
-    $solution_file = "$project_dir\Coderoom.sln"
-    $build_configuration = "Release"
-    $build_output_dir = "$project_dir\build-output"
+	$build_dir = split-path $psake.build_script_file
+	$project_dir = "$build_dir\.."
+	
+	$solution_file = "$project_dir\Coderoom.sln"
+	$build_configuration = "Release"
+	$build_output_dir = "$project_dir\build-output"
 }
 
 task default -depends compile
 
 FormatTaskName {
 	param($taskName)
-    write-host
+	write-host
 	write-host (("-"*30) + "[ " + $taskName.ToUpper() + " ]" + ("-"*30)) -fore Green
-    write-host
+	write-host
 }
 
 Task compile { 
-    msbuild $solution_file /m /property:"Configuration=$build_configuration;OutputPath=$build_output_dir" /nologo
+	try {
+		msbuild $solution_file /m /property:"Configuration=$build_configurationasdas;OutputPath=$build_output_dir" /nologo
+	}
+	catch {
+		write-output "##teamcity[buildStatus text='MSBuild Error - see build log for details' status='ERROR']"
+		write-error $_
+	}
 }
