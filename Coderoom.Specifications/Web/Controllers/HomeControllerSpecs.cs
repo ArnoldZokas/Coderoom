b4a44305a54dc046024f95cce47153952cfb168b
Arnold Zokas <arnold.zokas@coderoom.net>
2013-04-26 01:25:53 +0100

    Added specs for HomeController

---
+++ Coderoom.Specifications/Web/Controllers/HomeControllerSpecs.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Coderoom.Web.Controllers;
+using Machine.Specifications;
+
+namespace Coderoom.Specifications.Web.Controllers
+{
+	public class HomeControllerSpecs
+	{
+		public class when_home_page_is_requested
+		{
+			private static ActionResult result;
+
+			Because of = () => result = new HomeController().Index();
+
+			It should_display_home_page = () => result.ShouldNotBeNull();
+		} 
+	}
+}
